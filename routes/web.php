e0efcf3360767a1944b0500c8200d907ac9a7fd7
Vlad <vlad@nesutulsa.ru>
2021-03-24 22:45:45 +0300

    Add auth sanctum and allow route only for auth users

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get( '/', function () {
-    return view( 'welcome' );
-} );
 
 Auth::routes();
 
-Route::get( '/home', [ App\Http\Controllers\HomeController::class, 'index' ] )->name( 'home' );
+Route::middleware( [ 'auth:sanctum', 'verified' ] )->get( '/', function () {
+    return view( 'index' );
+} )->name( 'index' );
